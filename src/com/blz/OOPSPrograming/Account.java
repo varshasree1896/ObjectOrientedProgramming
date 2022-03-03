95dc9f5eefaf90c71726541ab531524b10e6d445
Pooja Sree <you@example.com>
2022-03-04 00:41:23 +0530

    Added:Welcome message

---
+++ src/com/blz/OOPSPrograming/Account.java
@@ -0,0 +1,8 @@
+package com.blz.OOPSPrograming;
+;
+public class Account {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Object Oriented programming");
+    }
+}
