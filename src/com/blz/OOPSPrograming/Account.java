8ad249fb06ec665cf9cd2819bfeacf63cd544861
Pooja Sree <you@example.com>
2022-03-04 00:54:59 +0530

    Added:AccountBalance

--- src/com/blz/OOPSPrograming/Account.java
+++ src/com/blz/OOPSPrograming/Account.java
@@ -1,8 +1,25 @@
 package com.blz.OOPSPrograming;
-;
+import java.util.Scanner;
 public class Account {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Object Oriented programming");
+        AccountTest. debit();
+
+    }
+}
+class AccountTest {
+    static long debitAmount;
+    static long accountBalance = 1000l;
+
+    public static void debit() {
+        Scanner sc = new Scanner(System. in);
+        System. out. println("Enter Debit amount");
+        debitAmount = sc. nextLong();
+        if (debitAmount > accountBalance) {
+            System. out. println("Debit amount exceeded account balance");
+            System. out. println("Account balance is = " + accountBalance);
+        } else
+            System. out. println("Debited amount is " + " " + debitAmount);
+        System. out. println("Account balance is " + (accountBalance - debitAmount));
     }
 }
